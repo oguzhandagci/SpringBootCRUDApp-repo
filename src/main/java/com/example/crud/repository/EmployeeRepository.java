6c3c07d8c80719fd23e0604c56710b4458bb3901
Ozzy Dagci <35441651+oguzhandagci@users.noreply.github.com>
2022-06-07 00:22:04 -0400

    First Commit

---
+++ src/main/java/com/example/crud/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.crud.repository;
+
+import com.example.crud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
